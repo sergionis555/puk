be24a663dab4499a315c4236bbd1e321c6ed47a3
PulkitSingh256 <psinghania256@gmail.com>
2017-06-30 20:28:46 +0530

    Typo Fix Goddamit

--- compile.bat
+++ compile.bat
@@ -1,3 +1,7 @@
-rem --specpath "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master" --distpath "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\dist" --workpath "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\build"
-pyinstaller --clean --upx-dir "upx393w" --noconsole --onefile "C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\RATAttack.py"
-"C:\Users\PULKIT\Desktop\NR-RAT-BETA-v0.1--master\dist\RATAttack.exe"
+@echo off
+echo ************************************************************************************************************************
+echo ************************************************NR_RAT_BETA EXE COMPILER************************************************
+echo ************************************************************************************************************************
+set /p id1 = Enter The Path of folder in which RAT_Attack.py is placed : 
+rem --specpath id1 --distpath "id1\dist" --workpath "id1\build"
+pyinstaller --clean --upx-dir "upx393w" --noconsole --onefile "RAT_Attack.py"
